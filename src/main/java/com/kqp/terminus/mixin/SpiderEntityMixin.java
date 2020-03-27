53fcfe2474df05fa016395435cd3abba96af5f1a
Kavin Phan <phan.kavin@gmail.com>
2020-03-27 16:04:55 -0400

    Add increased mob difficulty post-awakening

---
+++ src/main/java/com/kqp/terminus/mixin/SpiderEntityMixin.java
@@ -0,0 +1,51 @@
+package com.kqp.terminus.mixin;
+
+import com.kqp.terminus.Terminus;
+import com.kqp.terminus.util.MobDecorator;
+import jdk.internal.jline.internal.Nullable;
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnType;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.LocalDifficulty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpiderEntity.class)
+public abstract class SpiderEntityMixin {
+    @Inject(at = @At("TAIL"), method = "initAttributes")
+    protected void overrideAttributes(CallbackInfo callbackInfo) {
+        if (Terminus.worldProperties.isWorldAwakened()) {
+            SpiderEntity spider = (SpiderEntity) (Object) this;
+
+            spider.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED).setBaseValue(0.28D);
+            spider.getAttributeInstance(EntityAttributes.ARMOR).setBaseValue(6.0D);
+            spider.getAttributeInstance(EntityAttributes.ATTACK_DAMAGE).setBaseValue(12.0D);
+        }
+    }
+
+    @Inject(at = @At("RETURN"), method = "initialize")
+    public void addSkeletonRider(IWorld world, LocalDifficulty difficulty, SpawnType spawnType, @Nullable EntityData entityData, @Nullable CompoundTag entityTag, CallbackInfoReturnable callbackInfo) {
+        if (Terminus.worldProperties.isWorldAwakened()) {
+            SpiderEntity spider = (SpiderEntity) (Object) this;
+
+            if (spider.getPassengerList().size() == 0 && world.getRandom().nextFloat() < 0.75F) {
+
+                SkeletonEntity skeletonEntity = (SkeletonEntity) EntityType.SKELETON.create(spider.world);
+
+                skeletonEntity.refreshPositionAndAngles(spider.getX(), spider.getY(), spider.getZ(), spider.yaw, 0.0F);
+                skeletonEntity.initialize(world, difficulty, spawnType, (EntityData)null, (CompoundTag)null);
+                world.spawnEntity(skeletonEntity);
+                skeletonEntity.startRiding(spider);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/kqp/terminus/util/MobDecorator.java
@@ -0,0 +1,57 @@
+package com.kqp.terminus.util;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+import java.util.Random;
+
+public class MobDecorator {
+    public static void giveArmor(HostileEntity entity) {
+        Random r = entity.getRandom();
+
+        if (r.nextFloat() < 0.5F) {
+            if (r.nextFloat() < 0.75F)
+                entity.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.DIAMOND_HELMET));
+            if (r.nextFloat() < 0.9F)
+                entity.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.DIAMOND_CHESTPLATE));
+            if (r.nextFloat() < 0.75F)
+                entity.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.DIAMOND_LEGGINGS));
+            if (r.nextFloat() < 0.5F)
+                entity.equipStack(EquipmentSlot.FEET, new ItemStack(Items.DIAMOND_BOOTS));
+        } else {
+            if (r.nextFloat() < 0.75F)
+                entity.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.IRON_HELMET));
+            if (r.nextFloat() < 0.9F)
+                entity.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.IRON_CHESTPLATE));
+            if (r.nextFloat() < 0.75F)
+                entity.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.IRON_LEGGINGS));
+            if (r.nextFloat() < 0.5F)
+                entity.equipStack(EquipmentSlot.FEET, new ItemStack(Items.IRON_BOOTS));
+        }
+
+        if (r.nextFloat() < 0.75F && !entity.getEquippedStack(EquipmentSlot.HEAD).isEmpty())
+            EnchantmentHelper.enchant(r, entity.getEquippedStack(EquipmentSlot.HEAD), 25, false);
+        if (r.nextFloat() < 0.9F && !entity.getEquippedStack(EquipmentSlot.CHEST).isEmpty())
+            EnchantmentHelper.enchant(r, entity.getEquippedStack(EquipmentSlot.CHEST), 25, false);
+        if (r.nextFloat() < 0.75F && !entity.getEquippedStack(EquipmentSlot.LEGS).isEmpty())
+            EnchantmentHelper.enchant(r, entity.getEquippedStack(EquipmentSlot.LEGS), 25, false);
+        if (r.nextFloat() < 0.5F && !entity.getEquippedStack(EquipmentSlot.FEET).isEmpty())
+            EnchantmentHelper.enchant(r, entity.getEquippedStack(EquipmentSlot.FEET), 25, false);
+    }
+
+    public static void giveSword(HostileEntity entity) {
+        Random r = entity.getRandom();
+
+        if (r.nextFloat() < 0.75F) {
+            entity.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.DIAMOND_SWORD));
+        } else {
+            entity.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
+        }
+
+        if (r.nextFloat() < 0.95F && !entity.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty())
+            EnchantmentHelper.enchant(r, entity.getEquippedStack(EquipmentSlot.MAINHAND), 30, false);
+    }
+}
