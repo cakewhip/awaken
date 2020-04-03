91eb37e192390ac3afbca6a5d7f72d9c1c30a2a5
Kavin Phan <phan.kavin@gmail.com>
2020-04-03 08:28:22 -0400

    Reset blood moon chance

--- src/main/java/com/kqp/awaken/data/AwakenConfig.java
+++ src/main/java/com/kqp/awaken/data/AwakenConfig.java
@@ -6,5 +6,5 @@ package com.kqp.awaken.data;
 public class AwakenConfig {
     public static final long NIGHT_START = 12786;
     public static final long NIGHT_END = 23216;
-    public static final float BLOOD_MOON_CHANCE = 100F / 100F;
+    public static final float BLOOD_MOON_CHANCE = 5F / 100F;
 }
