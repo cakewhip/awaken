eb766bc799963dc521bcdc66eda4b59233ddd43e
Kavin Phan <phan.kavin@gmail.com>
2020-04-06 23:16:03 -0400

    Add skeleton blood moon texture

--- src/main/java/com/kqp/awaken/mixin/SkeletonEntityRendererMixin.java
+++ src/main/java/com/kqp/awaken/mixin/SkeletonEntityRendererMixin.java
@@ -1,8 +1,8 @@
 package com.kqp.awaken.mixin;
 
 import com.kqp.awaken.Awaken;
-import net.minecraft.client.render.entity.ZombieBaseEntityRenderer;
-import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.client.render.entity.SkeletonEntityRenderer;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 /**
  * Used to override the zombie texture during blood moons.
  */
-@Mixin(ZombieBaseEntityRenderer.class)
-public class ZombieBaseEntityRendererMixin {
-    private static final Identifier BLOOD_MOON_TEXTURE = new Identifier(Awaken.MOD_ID, "textures/entity/blood_moon/zombie.png");
+@Mixin(SkeletonEntityRenderer.class)
+public class SkeletonEntityRendererMixin {
+    private static final Identifier BLOOD_MOON_TEXTURE = new Identifier(Awaken.MOD_ID, "textures/entity/blood_moon/skeleton.png");
 
     @Inject(at = @At("HEAD"), method = "getTexture", cancellable = true)
-    private void overrideTextureForBloodMoon(ZombieEntity zombieEntity, CallbackInfoReturnable<Identifier> callbackInfo) {
+    private void overrideTextureForBloodMoon(AbstractSkeletonEntity abstractSkeletonEntity, CallbackInfoReturnable<Identifier> callbackInfo) {
         if (Awaken.worldProperties.isBloodMoonActive()) {
             callbackInfo.setReturnValue(BLOOD_MOON_TEXTURE);
         }
