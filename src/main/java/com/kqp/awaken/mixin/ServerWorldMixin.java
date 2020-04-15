499389766d6fd06b7710f772300a0baa72585a18
Kavin Phan <phan.kavin@gmail.com>
2020-04-15 10:55:26 -0400

    Isolate sleep triggers

--- src/main/java/com/kqp/awaken/mixin/ServerWorldMixin.java
+++ src/main/java/com/kqp/awaken/mixin/ServerWorldMixin.java
@@ -9,12 +9,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * Used to trigger sleep events.
+ * Used to:
+ * Trigger sleep events
  */
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin {
     @Inject(at = @At("HEAD"), method = "wakeSleepingPlayers")
     public void wakePlayers(CallbackInfo callbackInfo) {
+        doSleepTriggers();
+    }
+
+    private static void doSleepTriggers() {
         Awaken.worldProperties.triggers.forEach(trigger -> {
             if (trigger instanceof SleepTrigger) {
                 trigger.activate();
