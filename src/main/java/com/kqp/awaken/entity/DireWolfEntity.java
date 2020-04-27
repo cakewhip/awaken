abbf70641e0ed55ba5793c7897c71cf1f32d2ec2
Kavin Phan <phan.kavin@gmail.com>
2020-04-27 16:41:46 -0400

    Adjust attributes for direwolf

--- src/main/java/com/kqp/awaken/entity/DireWolfEntity.java
+++ src/main/java/com/kqp/awaken/entity/DireWolfEntity.java
@@ -11,7 +11,8 @@ import net.minecraft.entity.ai.goal.PounceAtTargetGoal;
 import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
-import net.minecraft.entity.ai.pathing.PathNodeType;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.MobNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -26,8 +27,14 @@ import net.minecraft.world.World;
 public class DireWolfEntity extends HostileEntity {
     public DireWolfEntity(World world) {
         super(AwakenEntities.DIRE_WOLF, world);
+    }
 
-        this.setPathfindingPenalty(PathNodeType.WATER, 0.0F);
+    public static DefaultAttributeContainer.Builder createDireWolfAttributes() {
+        return HostileEntity.createHostileAttributes()
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6D)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 40D)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48D)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12.0D);
     }
 
     @Override
@@ -39,17 +46,21 @@ public class DireWolfEntity extends HostileEntity {
         this.goalSelector.add(4, new AttackGoal(this));
         this.goalSelector.add(5, new WanderAroundFarGoal(this, 0.8D));
         this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
-        this.goalSelector.add(6, new LookAroundGoal(this));
+        this.goalSelector.add(7, new LookAroundGoal(this));
+
         this.targetSelector.add(1, new RevengeGoal(this));
         this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, false));
     }
 
-    public static DefaultAttributeContainer.Builder createDireWolfAttributes() {
-        return HostileEntity.createHostileAttributes()
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4D)
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 40D)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12.0D);
+    @Override
+    protected EntityNavigation createNavigation(World world) {
+        MobNavigation navigation = new MobNavigation(this, world);
+        navigation.setCanSwim(true);
+        navigation.setSpeed(0.6D);
+        navigation.setAvoidSunlight(false);
+        navigation.setCanPathThroughDoors(false);
+
+        return navigation;
     }
 
     @Override
@@ -58,7 +69,7 @@ public class DireWolfEntity extends HostileEntity {
             return false;
         } else {
             if (target instanceof LivingEntity) {
-                ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 1));
+                ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 2 * 20, 0));
             }
 
             return true;
