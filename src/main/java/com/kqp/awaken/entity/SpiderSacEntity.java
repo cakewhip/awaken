1d9b1ef2a7b71553ed8c7c192a207f38176fac74
Kavin Phan <phan.kavin@gmail.com>
2020-05-03 11:04:37 -0400

    Set spider sac knockback resistance to 100%

--- src/main/java/com/kqp/awaken/entity/SpiderSacEntity.java
+++ src/main/java/com/kqp/awaken/entity/SpiderSacEntity.java
@@ -29,7 +29,8 @@ public class SpiderSacEntity extends HostileEntity {
         return HostileEntity.createHostileAttributes()
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0D)
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 40D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0D);
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0D)
+                .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0D);
     }
 
     @Override
