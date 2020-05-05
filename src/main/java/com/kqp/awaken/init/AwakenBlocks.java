9b1e4fb1aea379f7dc63ab670bf1f8c2afda9a75
Kavin Phan <phan.kavin@gmail.com>
2020-05-04 21:53:33 -0400

    More comments

--- src/main/java/com/kqp/awaken/init/AwakenBlocks.java
+++ src/main/java/com/kqp/awaken/init/AwakenBlocks.java
@@ -32,10 +32,11 @@ public class AwakenBlocks {
     public static final Block ANCIENT_SALVIUM_ORE = register("ancient_salvium_ore", new AwakenOreBlock(ancientOreSettings(15F).build()));
     public static final Block ANCIENT_VALERIUM_ORE = register("ancient_valerium_ore", new AwakenOreBlock(ancientOreSettings(15F).build()));
 
-    public static final Block SALVIUM_ORE = register("salvium_ore", new AwakenOreBlock(stoneSettings(10F, 40F, 3).build()));
-    public static final Block VALERIUM_ORE = register("valerium_ore", new AwakenOreBlock(stoneSettings(10F, 40F, 3).build()));
+    // TODO: find out why vanilla pickaxes don't work for mining level
+    // as a result of the issue mentioned above, salvium and valerium will be minable by any pickaxe
+    public static final Block SALVIUM_ORE = register("salvium_ore", new AwakenOreBlock(stoneSettings(10F, 40F, 0).build()));
+    public static final Block VALERIUM_ORE = register("valerium_ore", new AwakenOreBlock(stoneSettings(10F, 40F, 0).build()));
 
-    // TODO: find out why vanilla pickaxes don't work
 
     public static final Block ENDERIAN_HELL_FORGE = register("enderian_hell_forge", new CraftingBlock(
             FabricBlockSettings.of(Material.STONE).strength(35F, 12F).lightLevel(12).build(),
