6845a7fa9521a4bb2ec94bc69f63dc4543286ddb
Kavin Phan <phan.kavin@gmail.com>
2020-05-29 13:30:11 -0400

    New experimental data-driven armor

--- src/main/java/com/kqp/awaken/item/material/AwakenArmorMaterial.java
+++ src/main/java/com/kqp/awaken/item/material/AwakenArmorMaterial.java
@@ -1,52 +1,47 @@
 package com.kqp.awaken.item.material;
 
-import com.kqp.awaken.init.AwakenItems;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonObject;
+import com.kqp.awaken.init.Awaken;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 /**
  * Used to create custom armor materials.
  */
 public class AwakenArmorMaterial implements ArmorMaterial {
-    public static final AwakenArmorMaterial DRAGON_SCALE = new AwakenArmorMaterial("dragon_scale", 37, new int[] { 6, 8, 10, 6 }, 14, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 4.0F, 0.5F, () -> {
-        return Ingredient.ofItems(AwakenItems.Reagents.ENDER_DRAGON_SCALE);
-    });
-
-    public static final AwakenArmorMaterial WITHER_BONE = new AwakenArmorMaterial("wither_bone", 37, new int[] { 6, 8, 10, 6 }, 14, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 4.0F, 0.5F, () -> {
-        return Ingredient.ofItems(AwakenItems.Reagents.WITHER_RIB);
-    });
-
-    public static final AwakenArmorMaterial SALVIUM = new AwakenArmorMaterial("salvium", 45, new int[] { 7, 9, 12, 7 }, 16, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 5.0F, 0.5F, () -> {
-        return Ingredient.ofItems(AwakenItems.Reagents.SALVIUM_INGOT);
-    });
-
-    public static final AwakenArmorMaterial VALERIUM = new AwakenArmorMaterial("valerium", 45, new int[] { 7, 10, 12, 7 }, 16, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 7.5F, 0.5F, () -> {
-        return Ingredient.ofItems(AwakenItems.Reagents.VALERIUM_INGOT);
-    });
-
     private static final int[] BASE_DURABILITY = new int[] { 13, 15, 16, 11 };
     private final String name;
     private final int durabilityMultiplier;
     private final int[] protectionAmounts;
     private final int enchantability;
-    private final SoundEvent equipSound;
+    private final Supplier<SoundEvent> equipSoundSupplier;
     private final float toughness;
     private final float knockbackResistance;
     private final Supplier<Ingredient> repairIngredientSupplier;
 
-    AwakenArmorMaterial(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> ingredientSupplier) {
+    private AwakenArmorMaterial(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, Supplier<SoundEvent> equipSoundSupplier, float toughness, float knockbackResistance, Supplier<Ingredient> ingredientSupplier) {
         this.name = name;
         this.durabilityMultiplier = durabilityMultiplier;
         this.protectionAmounts = protectionAmounts;
         this.enchantability = enchantability;
-        this.equipSound = equipSound;
+        this.equipSoundSupplier = equipSoundSupplier;
         this.toughness = toughness;
         this.knockbackResistance = knockbackResistance;
         this.repairIngredientSupplier = ingredientSupplier;
@@ -69,7 +64,7 @@ public class AwakenArmorMaterial implements ArmorMaterial {
 
     @Override
     public SoundEvent getEquipSound() {
-        return this.equipSound;
+        return this.equipSoundSupplier.get();
     }
 
     @Override
@@ -92,7 +87,77 @@ public class AwakenArmorMaterial implements ArmorMaterial {
         return this.toughness;
     }
 
-    public static AwakenArmorMaterial trinketMaterial(String name, int durability) {
-        return new AwakenArmorMaterial(name, durability, new int[] { 0, 0, 0, 0 }, 1, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0F, 0F, () -> Ingredient.EMPTY);
+    private static JsonDeserializer<AwakenArmorMaterial> DESERIALIZER = (json, typeOfT, context) -> {
+        JsonObject jsonArmorMaterial = json.getAsJsonObject();
+
+        // Easy to grab fields
+        String name = jsonArmorMaterial.get("name").getAsString();
+        int durabilityMultiplier = jsonArmorMaterial.get("durability_multiplier").getAsInt();
+        int enchantability = jsonArmorMaterial.get("enchantability").getAsInt();
+        float toughness = jsonArmorMaterial.get("toughness").getAsFloat();
+
+        // Protection amounts is stored in a JSON array, just need to iterate through first four
+        int[] protectionAmounts = new int[4];
+        JsonArray jsonProtectionAmounts = jsonArmorMaterial.getAsJsonArray("protection_amounts");
+        for (int i = 0; i < protectionAmounts.length; i++) {
+            protectionAmounts[i] = jsonProtectionAmounts.get(i).getAsInt();
+        }
+
+        String equipSoundString = jsonArmorMaterial.get("equip_sound").getAsString();
+        Identifier equipSoundId = new Identifier(equipSoundString);
+        Supplier<SoundEvent> equipSoundSupplier = () -> {
+            return Registry.SOUND_EVENT.get(equipSoundId);
+        };
+
+        // Repair ingredients are stored as string identifiers
+        JsonArray jsonRepairIngredients = jsonArmorMaterial.get("repair_ingredients").getAsJsonArray();
+        List<Identifier> repairIngredientIds = new ArrayList<Identifier>();
+
+        // Grab strings, map to Identifier objects, add to list of IDs
+        jsonRepairIngredients.forEach(repairIngredientElement -> {
+            repairIngredientIds.add(new Identifier(repairIngredientElement.getAsString()));
+        });
+
+        // Create supplier that will dynamically grab items and map them from item->itemstack->ingredient
+        // This is because it's uncertain whether items will be loaded when armor materials are loaded
+        Supplier<Ingredient> repairIngredientsSupplier = () -> {
+            return Ingredient.method_26964(repairIngredientIds
+                    .stream()
+                    .map(repairIngredientId -> Registry.ITEM.get(repairIngredientId))
+                    .map(ItemStack::new)
+            );
+        };
+
+        return new AwakenArmorMaterial(
+                name,
+                durabilityMultiplier,
+                protectionAmounts,
+                enchantability,
+                equipSoundSupplier,
+                toughness,
+                0.0F,
+                repairIngredientsSupplier
+        );
+    };
+
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(AwakenArmorMaterial.class, DESERIALIZER)
+            .create();
+
+    private static AwakenArmorMaterial fromJson(String name) {
+        String path = String.format("data/%s/item_stats/armor/%s.json", Awaken.MOD_ID, name);
+        System.out.println(AwakenArmorMaterial.class.getClassLoader().getResource(path).getFile());
+        InputStreamReader reader = new InputStreamReader(AwakenArmorMaterial.class.getClassLoader().getResourceAsStream(path));
+
+        return GSON.fromJson(reader, AwakenArmorMaterial.class);
     }
+
+    // TODO: move these to a registry and make armor items auto-magically pull from it
+    public static final AwakenArmorMaterial DRAGON_SCALE = fromJson("dragon_scale");
+
+    public static final AwakenArmorMaterial WITHER_BONE = fromJson("wither_bone");
+
+    public static final AwakenArmorMaterial SALVIUM = fromJson("salvium");
+
+    public static final AwakenArmorMaterial VALERIUM = fromJson("valerium");
 }
